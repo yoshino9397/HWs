7aad53f259a46a79c9332d0b05955e99cb01cbf2
yoshino9397 <yayamayoshino@gmail.com>
2021-10-25 20:39:49 -0700

    anyway

--- 1029Presen/main.java
+++ 1029Presen/main.java
@@ -1,24 +1,24 @@
-class dentaku {
+class calculator {
   public static void main(String args[]) {
-    int val1 = 0; /* 最初の数字 */
-    int val2 = 0; /* 次の数字 */
-    String ope; /* 演算子 */
-    int kekka; /* 結果を格納 */
+    int val1 = 0; /* First Number */
+    int val2 = 0; /* Second Number */
+    String ope; /* operator */
+    int result;
 
     if (args.length != 3) {
-      errDisp("引数は3個入力して下さい");
+      errDisp("Please enter 3 arguments.");
     }
 
     try {
       val1 = Integer.parseInt(args[0]);
       val2 = Integer.parseInt(args[2]);
     } catch (NumberFormatException e) {
-      errDisp("数値でない値が入力されています");
+      errDisp("A non-numeric value has been entered.");
     }
 
     ope = args[1];
 
-    String opeStr[] = { "kake", "waru", "tasu", "hiku" };
+    String opeStr[] = { "×", "÷", "＋", "-" };
     boolean errFlag = true;
 
     for (int i = 0; i < 4; i++) {
@@ -28,30 +28,29 @@ class dentaku {
     }
 
     if (errFlag == true) {
-      errDisp("演算子は kake waru tasu hiku の4つです");
+      errDisp("There are four operators.");
     }
 
-    if (ope.equals("kake")) {
-      kekka = val1 * val2;
-    } else if (ope.equals("tasu")) {
-      kekka = val1 + val2;
-    } else if (ope.equals("hiku")) {
-      kekka = val1 - val2;
+    if (ope.equals("×")) {
+      result = val1 * val2;
+    } else if (ope.equals("＋")) {
+      result = val1 + val2;
+    } else if (ope.equals("-")) {
+      result = val1 - val2;
     } else {
       if (val2 == 0) {
-        errDisp("0 で割ろうとしました");
+        errDisp("You tried to divide by 0.");
       }
 
-      kekka = val1 / val2;
+      result = val1 / val2;
     }
 
-    System.out.println("入力された式は " + val1 + " " + ope + " " + val2 + " です");
-    System.out.println("計算結果は " + kekka + " です");
+    System.out.println("You entered " + val1 + " " + ope + " " + val2 + ".");
+    System.out.println("The calculation result is " + result + ".");
   }
 
   private static void errDisp(String errStr) {
-    System.out.println("Usage : java dentaku 数値 演算子 数値");
     System.out.println(errStr);
-    System.exit(0); /* プログラムを終了する */
+    System.exit(0); /* Exit the program */
   }
 }
