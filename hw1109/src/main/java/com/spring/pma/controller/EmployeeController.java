f095f853b4fee4b48062f4f94728d985710c5809
yoshino9397 <yayamayoshino@gmail.com>
2021-11-09 11:33:59 -0800

    wakaranu

---
+++ hw1109/src/main/java/com/spring/pma/controller/EmployeeController.java
@@ -0,0 +1,34 @@
+package com.spring.pma.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.spring.pma.dao.iEmployeeRepository;
+import com.spring.pma.entity.Employee;
+
+@Controller
+@RequestMapping("/employees")
+public class EmployeeController {
+
+  @Autowired
+  iEmployeeRepository proRepo;
+
+  // @RequestMapping("/new")
+  @GetMapping("/new")
+  public String displayEmployeeForm(Model model) {
+    model.addAttribute("employee", new Employee());
+    return "employees/new-employee";
+  }
+
+  // @RequestMapping(value = "/save", method = RequestMethod.POST)
+  @PostMapping("/save")
+  public String createEmployee(Employee employee, Model model) {
+    // this is where we save to database
+    proRepo.save(employee);
+    return "redirect:/employees/new";
+  }
+}
---
+++ hw1109/src/main/java/com/spring/pma/dao/iEmployeeRepository.java
@@ -0,0 +1,14 @@
+package com.spring.pma.dao;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.spring.pma.entity.Employee;
+
+public class iEmployeeRepository extends CrudRepository <Employee, Long> {
+
+  @Override
+  public List<Employee> findAll();
+
+}
