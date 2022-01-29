8d2f048687534ab3fd890a6ff8cda146a56721d0
矢山佳乃 <yayamayoshino@yayamayoshinonoMacBook-Air.local>
2022-01-28 20:16:25 -0800

    anyway

--- 0128/solution.js
+++ 0128/solution.js
@@ -6,20 +6,42 @@ async function getData() {
   const response = await fetch(API_URL);
   const { results } = await response.json();
 
-  //clear result
-  result.innerHTML = ``;
+  result.innerHTML = "";
 
   const ul = document.createElement("ul");
-  container.className = "user-list";
-  results.map(() => {
+  ul.className = "user-list";
+
+  results.map((e) => {
     const li = document.createElement("li");
     const img = document.createElement("img");
-    const user = document.createElement("div");
     const name = document.createElement("h4");
-    const location = document.createElement("p");
+    const country = document.createElement("p");
+    const email = document.createElement("p");
 
-    img.setAttribute("src", results.picture.thumbnail);
+    img.setAttribute("src", e.picture.thumbnail);
+    name.textContent = e.name.first;
+    country.textContent = e.location.country;
+    email.textContent = e.email;
+
+    li.appendChild(img);
+    li.appendChild(name);
+    li.appendChild(country);
+    li.appendChild(email);
+    ul.appendChild(li);
   });
+  result.appendChild(ul);
 }
+
+filter.addEventListener("keyup", function (e) {
+  let searchChar = e.target.value.toLowerCase();
+  let users = result.getElementsByTagName("li");
+
+  Array.from(users).forEach((user) => {
+    let parText = user.children[1].textContent;
+    if (parText.toLowerCase().indexOf(searchChar) !== -1) {
+      user.classList.remove("hide");
+    } else user.classList.add("hide");
+  });
+});
 
 getData();
