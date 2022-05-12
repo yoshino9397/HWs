6851ea7b38449d5f36f186f9a2d2c3db68d666c1
矢山佳乃 <yayamayoshino@yayamayoshinonoMacBook-Air.local>
2022-05-12 12:32:03 -0700

    first commit

---
+++ 0512/server.js
@@ -0,0 +1,22 @@
+const path = require('path')
+const express = require('express')
+const bodyParser = require('body-parser')
+const res = require('express/lib/response')
+
+/* ---------------------------- setups and config --------------------------- */
+const app = express()
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, 'public')))
+
+
+/* ------------------------------- middleware ------------------------------- */
+app.use('/api/members', require('./routes/members'))
+
+app.get('/', (req,res) => res.json({ msg: 'This is working... '}))
+
+app.use((req,res) => res.sendFile(path.join(__dirname, 'public', '404.html')))
+
+/* -------------------------------- listener -------------------------------- */
+const PORT = process.env.PORT || 8000 
+app.listen(PORT)
