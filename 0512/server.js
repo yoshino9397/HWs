b3f4bb3337c3b9b96a1e2cf201d9d5958238175d
矢山佳乃 <yayamayoshino@yayamayoshinonoMacBook-Air.local>
2022-05-12 15:29:48 -0700

    DONE

--- 0512/server.js
+++ 0512/server.js
@@ -1,22 +1,26 @@
-const path = require('path')
-const express = require('express')
-const bodyParser = require('body-parser')
-const res = require('express/lib/response')
+const path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
+const res = require("express/lib/response");
+const routes = require("./routes/members");
 
 /* ---------------------------- setups and config --------------------------- */
-const app = express()
+const app = express();
+app.use(routes);
+app.set("view engine", "ejs");
+app.set("views", "routes");
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(express.static(path.join(__dirname, 'public')))
-
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 
 /* ------------------------------- middleware ------------------------------- */
-app.use('/api/members', require('./routes/members'))
+app.use("/api/members", require("./routes/members"));
 
-app.get('/', (req,res) => res.json({ msg: 'This is working... '}))
-
-app.use((req,res) => res.sendFile(path.join(__dirname, 'public', '404.html')))
+app.get("/", (req, res) => {
+  res.render("index", { pageTitle: "Home Page" });
+});
+app.use((req, res) => res.sendFile(path.join(__dirname, "public", "404.html")));
 
 /* -------------------------------- listener -------------------------------- */
-const PORT = process.env.PORT || 8000 
-app.listen(PORT)
+const PORT = process.env.PORT || 8000;
+app.listen(PORT);
