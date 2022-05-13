56f154b2fedd9a7b363a8fd8b5daaa95eda2f07f
矢山佳乃 <yayamayoshino@yayamayoshinonoMacBook-Air.local>
2022-05-13 14:21:20 -0700

    DONE

--- 0513/src/routes/index.js
+++ 0513/src/routes/index.js
@@ -1,27 +1,53 @@
-const router = require('express').Router()
-const db = require('../services/dbsqlite')
+const router = require("express").Router();
+const db = require("../services/dbsqlite");
 
-router.get('/',  (req, res) => {
-    const sql = "SELECT * FROM Todos ORDER BY Title"
-    db.all(sql, [], (err, rows) => {
-        if(err) return console.error(err.message)
+router.get("/", (req, res) => {
+  const sql = "SELECT * FROM Todos ORDER BY Title";
+  db.all(sql, [], (err, rows) => {
+    if (err) return console.error(err.message);
 
-        res.render('index', { model:rows })
-    })
-})
+    res.render("index", { model: rows });
+  });
+});
 
-router.get('/create', (req, res) => {
-    res.render('create', { model: {} })
-})
-router.post('/create', (req, res) => {
-    const sql = "INSERT INTO Todos (Title) VALUES (?)"
-    const todo = [req.body.Title]
+router.get("/create", (req, res) => {
+  res.render("create", { model: {} });
+});
+router.post("/create", (req, res) => {
+  const sql = "INSERT INTO Todos (Title) VALUES (?)";
+  const todo = [req.body.Title];
+  db.run(sql, todo, (err) => {
+    if (err) return console.error(err.message);
 
-    db.run(sql, todo, (err) => {
-        if(err) return console.error(err.message)
+    res.redirect("/");
+  });
+});
 
-        res.redirect('/')
-    })
-})
+router.get("/edit/:id", (req, res) => {
+  const sql = "SELECT * FROM Todos WHERE ID = ?";
+  const editId = req.params.id;
+  db.get(sql, editId, (err, row) => {
+    if (err) return console.error(err.message);
+    res.render("edit", { model: row });
+  });
+});
+router.post("/edit/:id", (req, res) => {
+  const editId = req.params.id;
+  const sql = `UPDATE Todos SET Title = ? WHERE ID = ${editId}`;
+  const todo = [req.body.Title];
+  db.run(sql, todo, (err) => {
+    if (err) return console.error(err.message);
+    res.redirect("/");
+  });
+});
 
-module.exports = router
+router.get("/delete/:id", (req, res) => {
+  const sql = "DELETE FROM Todos WHERE ID = ?";
+  const deleteId = req.params.id;
+  db.run(sql, deleteId, (err) => {
+    if (err) return console.error(err.message);
+    res.redirect("/");
+  });
+});
+
+module.exports = router;
